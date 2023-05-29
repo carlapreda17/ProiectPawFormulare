06d78ef9e7b50fd26bc43fd291b0c7320001c24d
carla17preda <carla_preda@yahoo.com>
2023-05-29 18:21:43 +0300

    Varianta finala!

--- ProiectPawFormulare/Form5.Designer.cs
+++ ProiectPawFormulare/Form5.Designer.cs
@@ -34,6 +34,8 @@
             // 
             // textBox1
             // 
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI Black", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.textBox1.ForeColor = System.Drawing.Color.Indigo;
             this.textBox1.Location = new System.Drawing.Point(34, 34);
             this.textBox1.Margin = new System.Windows.Forms.Padding(4, 5, 4, 5);
             this.textBox1.Multiline = true;
